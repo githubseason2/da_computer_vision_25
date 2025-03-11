c91ae9e3bac0734811a83d61ab0a575cf40fd0d8
githubseason2 <111132990+githubseason2@users.noreply.github.com>
2025-03-11 23:22:20 +0900

    1주차 과제 완성 1트

--- 1주차 실습/2.웹캠 영상에서 에지 검출.py
+++ 1주차 실습/2.웹캠 영상에서 에지 검출.py
@@ -1,25 +1,25 @@
 import cv2 as cv
+import numpy as np
 import sys
 
 cap=cv.VideoCapture(0, cv.CAP_DSHOW)
-cv2.namedWindow('frame', cv2.WINDOW_NORMAL) 
+cv.namedWindow('frame') 
 while True:
     ret, frame = cap.read()
     if not ret:
         print("이미지를 불러오는데 실패했습니다.")
         break
-    key=cv.waitKey(1)
-    gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    extratedEdge=cv.Canny(gray_img, 110, 350)
+    gray_img = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    extratedEdge=cv.Canny(gray_img, 50, 50)
 
     tmp = np.stack( (extratedEdge, extratedEdge, extratedEdge), axis=2)
-    both_imgs = np.hstack((img, tmp))
+    both_imgs = np.hstack((frame, tmp))
 
     key=cv.waitKey(1)
     if key==ord('q'):
         cv.destroyAllWindows()
         break
-    cv2.imshow('frame', both_image)
+    cv.imshow('frame', both_imgs)
 cap.release()
 
 
